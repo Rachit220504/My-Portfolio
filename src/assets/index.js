b19e60a045794b8a2211b57eb89e36285b58eebe
Rachit220504 <129960130+Rachit220504@users.noreply.github.com>
2025-03-09 17:35:31 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,64 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import frontend from "./frontend.png";
+import cloud from "./cloud.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import c from "./tech/c.png";
+import cpp from "./tech/cpp.png";
+import php from "./tech/php.png";
+import postgresql from "./tech/postgresql.png";
+import python from "./tech/python.png";
+
+
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import teeDesign from "./teeDesign.png";
+import lms from "./lms.png";
+import keeper from "./keeper.png";
+
+export {
+  logo,
+  backend,
+  frontend,
+  cloud,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  tailwind,
+  threejs,
+  postgresql,
+  c,
+  cpp,
+  python,
+  php,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  teeDesign,
+  lms,
+  keeper,
+};
